091d0bb5a2339ceb19e3ef3ad6e70d4381c4a2bd
guang <quark_gluon_plasma@163.com>
2021-12-30 11:01:26 +0800

    feat: treeshking-visalize

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 chrome devtools 练习
 
 [用 worker 做网页性能优化](./worker-performance-optimization)
+
+
+# 运行
+
+
+执行 ```npx http-server .``` 启动一个静态服务，然后访问对应的页面，用 chrome devtools 调试即可。
---
+++ threeshking-visualize/index.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b;
+} 
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+const res = add(3, 5);
+console.log(res);
+
+const res2 = multiply(2, 4);
+console.log(res2);
